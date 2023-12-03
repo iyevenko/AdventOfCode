19378457759858ba73318399c4265af1033e8f58
Ivan Yevenko <iyevenko@gmail.com>
2023-12-03 12:57:00 -0500

    update 2023 day 1

--- 2023/01.py
+++ 2023/01.py
@@ -28,23 +28,17 @@ def solve2(f):
     return s
 
 
-def solve2_fancy():
+def solve2_fancy(f):
     # REGEX VERSION
     nums = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'zero': 0}
     s = 0
     for line in open(f).read().splitlines():
         pattern = r'(?=(\d|' + '|'.join(nums.keys()) + r'))'
         matches = re.findall(pattern, line)
-        print(line, matches)
         m1, m2 = matches[0], matches[-1]
-        if m1 in nums:
-            s += 10*nums[m1]
-        else:
-            s += 10*int(m1)
-        if m2 in nums:
-            s += nums[m2]
-        else:
-            s += int(m2)
+        m1 = int(m1) if m1.isdigit() else nums[m1]
+        m2 = int(m2) if m2.isdigit() else nums[m2]
+        s += 10 * m1 + m2
     return s
 
 if __name__ == '__main__':
@@ -52,3 +46,4 @@ if __name__ == '__main__':
     print(solve('01.txt'))
     # print(solve2('01_test2.txt'))
     print(solve2('01.txt'))
+    # print(solve2_fancy('01.txt'))
