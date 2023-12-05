1e36a9ac95cee210900195f92e05c17429a06ecd
Ivan Yevenko <iyevenko@gmail.com>
2023-12-05 02:59:29 -0500

    cleanup 2023 day 5

--- 2023/05.py
+++ 2023/05.py
@@ -1,22 +1,23 @@
 def solve(f):
+    # 321st place!!
     text = open(f).read()
     seeds, *map_strings = text.split('\n\n')
 
     seeds = [int(x) for x in seeds.split(': ')[1].split(' ')]
-    maps = []
+    mappings = []
     for map_str in map_strings:
-        map = {}
+        mapping = []
         for line in map_str.splitlines()[1:]:
             dst, src, n = [int(x) for x in line.split(' ')]
-            map[(src, src+n-1)] = (dst, dst+n-1)
-        maps.append(map)
+            mapping.append((src, src+n-1, dst, dst+n-1))
+        mappings.append(mapping)
 
     min_seed = 10000000000000000
     for seed in seeds:
-        for map in maps:
-            for src, dst in map.items():
-                if src[0] <= seed <= src[1]:
-                    seed = dst[0] + seed - src[0]
+        for mapping in mappings:
+            for src1, src2, dst1, dst2 in mapping:
+                if src1 <= seed <= src2:
+                    seed = dst1 + seed - src1
                     break
         if seed < min_seed:
             min_seed = seed
@@ -27,21 +28,22 @@ def solve2(f):
     text = open(f).read()
     fake_seeds, *map_strings = text.split('\n\n')
 
-    maps = []
+    mappings = []
     for map_str in map_strings:
-        map = {}
+        mapping = []
         for line in map_str.splitlines()[1:]:
             dst, src, n = [int(x) for x in line.split(' ')]
-            map[(src, src+n-1)] = (dst, dst+n-1)
-        maps.append(sorted(map.items()))
+            mapping.append((src, src+n-1, dst, dst+n-1))
+        mapping.sort()
+        mappings.append(mapping)
 
     fake_seeds = [int(x) for x in fake_seeds.split(': ')[1].split(' ')]
-    seeds = [(start, start + num-1) for start, num in zip(fake_seeds[::2], fake_seeds[1::2])]
+    seeds = [(start, start+num-1) for start, num in zip(fake_seeds[::2], fake_seeds[1::2])]
 
-    for map in maps:
+    for mapping in mappings:
         new_seeds = []
         for (start, end) in seeds:
-            for (src1, src2), (dst1, dst2) in map:
+            for src1, src2, dst1, dst2 in mapping:
                 if start < src1:
                     if end < src1:
                         new_seeds.append((start, end))
